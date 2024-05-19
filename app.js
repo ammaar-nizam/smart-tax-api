bfb01405aae207e70beec307f4d74ad28c129098
ammaar-nizam <ammaar.nizam@outlook.com>
2024-05-19 13:43:47 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -29,11 +29,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server is working!");
