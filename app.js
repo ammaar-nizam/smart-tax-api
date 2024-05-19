a69688a866ebd6b37b548667b085617b5e0951f9
ammaar-nizam <ammaar.nizam@outlook.com>
2024-05-19 14:27:30 +0530

    debugging

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.json());
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.raw({ type: 'application/json' }));
