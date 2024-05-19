d9ab9452d38391486e61771b53cdb8bbbd68f8f9
ammaar-nizam <ammaar.nizam@outlook.com>
2024-05-19 16:11:43 +0530

    chnages

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 const prisma = require("./config/prismaConfig");
 const express = require("express");
 const app = express();
